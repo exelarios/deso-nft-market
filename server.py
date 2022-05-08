9d045314e03a057dadc7109e7899017e2211e34f
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2022-05-08 12:22:27 -0700

    added endpoints for node.deso.org

--- server.py
+++ server.py
@@ -16,10 +16,15 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 nfts = db["nfts"]
 
+NODE_DESO = "https://node.deso.org"
+
 # DeSo Node
-deso_node_URL = "http://localhost:" + os.environ.get("DESO_PORT")
-baseURL = deso_node_URL + "/api/v0"
-# http://localhost:18001/api/v0/
+NODE_LOCAL = "http://localhost:" + os.environ.get("DESO_PORT")
+baseURL = NODE_DESO + "/api"
+
+headers = {
+  "Content-Type": "application/json"
+}
 
 @app.route("/")
 def index():
@@ -28,30 +33,67 @@ def index():
 # Great way to check if the deso-node server is working.
 @app.route("/api/get-exchange-rate")
 def get_exchange_rate():
-  rates = requests.get(baseURL + "/get-exchange-rate")
+  rates = requests.get(baseURL + "/v0/get-exchange-rate")
   return jsonify(rates.json())
 
 @app.route("/api/get-single-profile", methods=["POST"])
 def get_single_profile():
-  profile = requests.get(baseURL + "/get-single-profile")
-  print(profile)
+  body = json.dumps(request.json)
+  profile = requests.post(baseURL + "/v0/get-single-profile", headers=headers, data=body)
   return jsonify(profile.json())
+
+@app.route("/api/balance", methods=["POST"])
+def get_user_balance():
+  body = json.dumps(request.json)
+  balance = requests.post(baseURL + "/v1/balance", headers=headers, data=body)
+  return jsonify(balance.json())
+
+@app.route("/api/update-profile", methods=["POST"])
+def update_profile():
+  body = json.dumps(request.json)
+  profile = requests.post(baseURL + "/v0/update-profile", headers=headers, data=body)
+  return jsonify(profile.json())
+  
+@app.route("/api/create-nft", methods=["POST"])
+def create_nft():
+  try:
+    body = json.dumps(request.json)
+    print(body)
+    nft = requests.post(baseURL + "/v0/create-nft", headers=headers, data=body)
+    return jsonify(nft.json())
+  except requests.exceptions.RequestException as error:
+    print(error)
+    raise SystemExit(error)
+
+@app.route("/api/submit-transaction", methods=["POST"])
+def submit_transaction():
+  body = json.dumps(request.json)
+  transaction = requests.post(baseURL + "v0/submit-transaction")
+  return jsonify(transaction.json())
+
+@app.route("/api/get-posts-stateless", methods=["POST"])
+def get_posts_stateless():
+  body = json.dumps(request.json)
+  posts = requests.post(baseURL + "/v0/get-posts-stateless", headers=headers, data=body)
+  return jsonify(posts.json())
+
+@app.route("/api/get-posts-for-public-key", methods=["POST"])
+def get_posts_by_public_key():
+  body = json.dumps(request.json)
+  posts = requests.post(baseURL + "/v0/get-posts-for-public-key", headers=headers, data=body)
+  return jsonify(posts.json())
 
 @app.route("/api/submit-post", methods=["POST"])
 def submit_post():
   if request.method == "POST":
     print(request.json)
     payload = json.dumps(request.json)
-    headers = {
-      "Content-Type": "application/json"
-    }
     post = requests.post("http://localhost:18001/api/v0/submit-post", headers=headers ,data=payload)
     print(post.json())
     return jsonify(post.json())
 
 @app.route("/nft", methods=["GET"])
 def nft():
-
   cursor = nfts.find({})
   list_cursor = list(cursor)
   json_data = dumps(list_cursor)
