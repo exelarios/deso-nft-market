b72a87634897c264eb1ddaa9d4703c42c0ed461f
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2022-05-08 20:41:08 -0700

    added error handling to the endpoints

--- server.py
+++ server.py
@@ -38,60 +38,93 @@ def get_exchange_rate():
 
 @app.route("/api/get-single-profile", methods=["POST"])
 def get_single_profile():
-  body = json.dumps(request.json)
-  profile = requests.post(baseURL + "/v0/get-single-profile", headers=headers, data=body)
-  return jsonify(profile.json())
+  try:
+    body = json.dumps(request.json)
+    profile = requests.post(baseURL + "/v0/get-single-profile", headers=headers, data=body)
+    return jsonify(profile.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
 @app.route("/api/balance", methods=["POST"])
 def get_user_balance():
-  body = json.dumps(request.json)
-  balance = requests.post(baseURL + "/v1/balance", headers=headers, data=body)
-  return jsonify(balance.json())
+  try:
+    body = json.dumps(request.json)
+    balance = requests.post(baseURL + "/v1/balance", headers=headers, data=body)
+    return jsonify(balance.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
 @app.route("/api/update-profile", methods=["POST"])
 def update_profile():
-  body = json.dumps(request.json)
-  profile = requests.post(baseURL + "/v0/update-profile", headers=headers, data=body)
-  return jsonify(profile.json())
+  try:
+    body = json.dumps(request.json)
+    profile = requests.post(baseURL + "/v0/update-profile", headers=headers, data=body)
+    return jsonify(profile.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
   
 @app.route("/api/create-nft", methods=["POST"])
 def create_nft():
   try:
     body = json.dumps(request.json)
-    print(body)
     nft = requests.post(baseURL + "/v0/create-nft", headers=headers, data=body)
     return jsonify(nft.json())
   except requests.exceptions.RequestException as error:
-    print(error)
-    raise SystemExit(error)
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
 @app.route("/api/submit-transaction", methods=["POST"])
 def submit_transaction():
-  body = json.dumps(request.json)
-  transaction = requests.post(baseURL + "v0/submit-transaction")
-  return jsonify(transaction.json())
+  try:
+    body = json.dumps(request.json)
+    transaction = requests.post(baseURL + "v0/submit-transaction")
+    return jsonify(transaction.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
 @app.route("/api/get-posts-stateless", methods=["POST"])
 def get_posts_stateless():
-  body = json.dumps(request.json)
-  posts = requests.post(baseURL + "/v0/get-posts-stateless", headers=headers, data=body)
-  return jsonify(posts.json())
+  try:
+    body = json.dumps(request.json)
+    posts = requests.post(baseURL + "/v0/get-posts-stateless", headers=headers, data=body)
+    return jsonify(posts.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
 @app.route("/api/get-posts-for-public-key", methods=["POST"])
 def get_posts_by_public_key():
-  body = json.dumps(request.json)
-  posts = requests.post(baseURL + "/v0/get-posts-for-public-key", headers=headers, data=body)
-  return jsonify(posts.json())
+  try:
+    body = json.dumps(request.json)
+    posts = requests.post(baseURL + "/v0/get-posts-for-public-key", headers=headers, data=body)
+    return jsonify(posts.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
 @app.route("/api/submit-post", methods=["POST"])
 def submit_post():
-  if request.method == "POST":
-    print(request.json)
+  try:
     payload = json.dumps(request.json)
-    post = requests.post("http://localhost:18001/api/v0/submit-post", headers=headers ,data=payload)
-    print(post.json())
+    post = requests.post(baseURL + "/v0/submit-post", headers=headers ,data=payload)
     return jsonify(post.json())
+  except requests.exceptions.RequestException as error:
+    return jsonify({
+      "error": "{}".format(error)
+    })
 
+# Not a real endpoint, just for testing mongodb.
 @app.route("/nft", methods=["GET"])
 def nft():
   cursor = nfts.find({})
